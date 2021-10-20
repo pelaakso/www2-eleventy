eff71f5495e2e6650bfac6ae26fb41d283c9fb7d
pelaakso <pelaakso@users.noreply.github.com>
2021-10-20 17:49:09 +0300

    #68 Remove files that are not needed after all.

--- website/src/lib/hashCss.js
+++
@@ -1,9 +0,0 @@
-const glob = require('fast-glob');
-const hashing = require('./hashing');
-
-function hashCssFilesSync(dir) {
-    const cssFiles = glob.sync([`${dir}/**/*.css`]);
-    return hashing.generateHashOfFilesSync(cssFiles);
-};
-
-exports.hashCssFilesSync = hashCssFilesSync;
