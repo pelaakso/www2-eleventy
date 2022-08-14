f149689c8454b83086bc3eedfc48ce67b998d12e
pelaakso <pelaakso@users.noreply.github.com>
2022-08-14 19:40:22 +0300

    Add stack description

--- infra/bin/infra.ts
+++ infra/bin/infra.ts
@@ -6,6 +6,7 @@ import { InfraStack } from '../lib/infra-stack';
 const app = new cdk.App();
 
 new InfraStack(app, 'Www2EleventyInfraStack', {
+  description: 'Test site for Eleventy static site generator',
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
